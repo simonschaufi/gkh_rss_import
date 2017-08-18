7d8f6d92b007d947271cb143fd86e2e7cf3883fb
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2017-08-18 12:39:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gkh_rss_import
---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
+}
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'pi1/class.tx_gkhrssimport_pi1.php', '_pi1', 'list_type', 1);
