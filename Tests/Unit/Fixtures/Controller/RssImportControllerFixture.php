835606c23f4c7e69472a22be1d9c0e15bf579bc6
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2021-05-01 01:18:32 +0200

    [BUGFIX] Invalid cache identifier for image with parameters
    Resolves #13

---
+++ Tests/Unit/Fixtures/Controller/RssImportControllerFixture.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * (c) Gert Kaae Hansen, Simon Schaufelberger
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace GertKaaeHansen\GkhRssImport\Tests\Unit\Fixtures\Controller;
+
+use GertKaaeHansen\GkhRssImport\Controller\RssImportController;
+
+class RssImportControllerFixture extends RssImportController
+{
+    public function getFileExtensionFromUrl(string $url): string
+    {
+        return parent::getFileExtensionFromUrl($url);
+    }
+}
