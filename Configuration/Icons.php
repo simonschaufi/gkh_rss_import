897d19b69c15af88688c113c676072456bf8ac43
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2022-06-09 23:35:40 +0200

    [TASK] Import namespace

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,8 +1,10 @@
 <?php
 
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
 return [
     'ext-gkhrssimport-wizard-icon' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => 'EXT:gkh_rss_import/Resources/Public/Icons/Extension.svg'
     ],
 ];
